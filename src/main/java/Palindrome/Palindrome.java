e3180ea6936c44742a416a779eaabec8339d32be
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2019-01-29 16:27:19 +0200

    rooting project

--- src/main/java/Palindrome/Palindrome.java
+++ src/main/java/Palindrome/Palindrome.java
@@ -1,14 +1,17 @@
+package Palindrome;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class Main {
+class Palindrome {
     private static boolean isPrime(int n) {
         if (n < 2) return false;
         if (n == 2 || n == 3) return true;
         if (n % 2 == 0 || n % 3 == 0) return false;
         var sqrtN = Math.sqrt(n) + 1;
-        return IntStream.iterate(6, i -> i <= sqrtN, i -> i + 6)
+        return IntStream
+                .iterate(6, i -> i <= sqrtN, i -> i + 6)
                 .noneMatch(i -> n % (i - 1) == 0 || n % (i + 1) == 0);
     }
 
@@ -18,8 +21,11 @@ class Main {
     }
 
     private static long[] program() {
-        var primeNumbers = IntStream.iterate(99999, i -> i >= 10000, i -> i - 1)
-                .filter(Main::isPrime).boxed().collect(Collectors.toList());
+        var primeNumbers = IntStream
+                .iterate(99999, i -> i >= 10000, i -> i - 1)
+                .filter(Palindrome::isPrime)
+                .boxed()
+                .collect(Collectors.toList());
 
         var response = new long[3];
         while (primeNumbers.size() != 0) {
